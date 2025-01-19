493827f291b8d4faf927c2a1087c75e40e62ea89
mishrayush15 <ayushmishra780193@gmail.com>
2025-01-19 18:19:27 +0530

    updated next config v2

--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -1,24 +1,24 @@
-'use client'
+    'use client'
 
-import { X } from "lucide-react";
-import Link from "next/link";
+    import { X } from "lucide-react";
+    import Link from "next/link";
 
-//as when we used the reset button with a reset function in the SearchForm component which is a server side component, it was showing an error as it can render a client side component that was our reset button on a server componenet. Hence to fix this we have created another client component that only exports the resetting funtionality
+    //as when we used the reset button with a reset function in the SearchForm component which is a server side component, it was showing an error as it can render a client side component that was our reset button on a server componenet. Hence to fix this we have created another client component that only exports the resetting funtionality
 
-export default function SearchFormReset() {
+    export default function SearchFormReset() {
 
-    const reset = () => {
-        const form = document.querySelector('.search-form') as HTMLFormElement;
+        const reset = () => {
+            const form = document.querySelector('.search-form') as HTMLFormElement;
 
-        if(form) form.reset();
+            if(form) form.reset();
+        }
+
+        return (
+            <button type='reset' onClick={reset}>
+                <Link href="/" className="search-btn text-white">
+                    <X className="size-5"/>
+                </Link>
+                
+            </button>
+        )
     }
-
-    return (
-        <button type='reset' onClick={reset}>
-            <Link href="/" className="search-btn text-white">
-                <X className="size-5"/>
-            </Link>
-            
-        </button>
-    )
-}
